7003a26e4fd10ac24c543eb3c3cc2b85e8e0feec
Oscar Lima <oscar.lima@dfki.de>
2023-11-21 14:41:14 +0100

    adapt template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Grasplan'
+copyright = '2023, Oscar Lima'
+author = 'Oscar Lima'
 
 release = '0.1'
 version = '0.1.0'
